645eee2fe47cfd8063624f9ad37092a224785942
geshunxin <geshunxin@163.com>
2018-12-15 00:59:34 +0800

    Initial Maven Project - pom.xml

---
+++ src/main/java/com/github/gsx/Application.java
@@ -0,0 +1,11 @@
+package com.github.gsx;
+
+// 类名必须和文件名一致
+public class Application
+{
+    // 入口函数, 签名必须这样写
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome come to JAVA...");
+    }
+}
